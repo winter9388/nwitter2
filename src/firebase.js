ae8e1ba8b987f8a8d571d2c347fbe5945ca5c084
winter9388 <winter9388@naver.com>
2021-11-13 17:40:14 +0900

    1.1 End of phase 2

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBp7MXrmfUYrjqNznzGoyZ2nhjEfGp7tTo",
+	authDomain: "nwitter-a92c1.firebaseapp.com",
+	projectId: "nwitter-a92c1",
+	storageBucket: "nwitter-a92c1.appspot.com",
+	messagingSenderId: "357637617382",
+	appId: "1:357637617382:web:2a7927e60d03cd32296d27",
+};
+
+export default firebase.initializeApp(firebaseConfig);

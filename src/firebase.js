dae1312a3c3f94360cabb8f0c5be3dc7116ae48d
winter9388 <winter9388@naver.com>
2021-11-13 18:57:08 +0900

    1.2 End of Phase 3

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,14 @@
 import firebase from "firebase/compat/app";
 
+//セキュリティキー保安のため、.envから各種キーを引っ張ってくる。　.envファイルはgithubにアップロードしない　（.gitignoreにリスト化）
+//비밀키 보안을 위해 .env파일에서 각종 보안키를 변수를 통해 가져옴, .env파일은 깃허브에 업로즈 하지 않는 방식으로 보안강화 (.gitignore)
 const firebaseConfig = {
-	apiKey: "AIzaSyBp7MXrmfUYrjqNznzGoyZ2nhjEfGp7tTo",
-	authDomain: "nwitter-a92c1.firebaseapp.com",
-	projectId: "nwitter-a92c1",
-	storageBucket: "nwitter-a92c1.appspot.com",
-	messagingSenderId: "357637617382",
-	appId: "1:357637617382:web:2a7927e60d03cd32296d27",
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);

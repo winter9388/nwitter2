3e0b2e7613d7495c278e5087de8e1d56cd535bee
winter9388 <winter9388@naver.com>
2021-11-14 18:10:42 +0900

    1.6 End of Phase 4-6 / Create Log Out System

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,20 +1,29 @@
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
 	return (
 		<Router>
+			{isLoggedIn && <Navigation />}
 			<Switch>
 				{isLoggedIn ? (
-					<Route exact path="/">
-						<Home />
-					</Route>
+					<>
+						<Route exact path="/">
+							<Home />
+						</Route>
+						<Route exact path="/profile">
+							<Profile />
+						</Route>
+					</>
 				) : (
 					<Route exact path="/">
 						<Auth />
 					</Route>
 				)}
+				{/* <Redirect from="*" to="/" />  Redirectを活用したLog out後のrootページへ遷移コード(redirectのimportが必要)*/}
 			</Switch>
 		</Router>
 	);
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,19 @@
-const Profile = () => <span>Profile</span>;
+import { authService } from "fbase";
+import { useHistory } from "react-router"; //Log out後rootページ遷移のためimport
+
+const Profile = () => {
+	const history = useHistory();
+
+	const onLogOutClick = () => {
+		authService.signOut();
+		history.push("/"); //jsコードを活用したLog out後rootページ遷移
+	};
+
+	return (
+		<>
+			<button onClick={onLogOutClick}>Log Out</button>
+		</>
+	);
+};
 
 export default Profile;
